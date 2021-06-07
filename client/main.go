2e6a68c7252f3bbb3448a625f45ebba15070d2f5
eduarrrd <>
2021-06-07 16:20:16 +0200

    client: timeouts, logging, flags, exit
    * bug: flags were never evaluated
    * logging: track connection open/closes
    * only attempt to connect for a short period, rather than indefinitely
    * read as blocker: conn.Read allows us to gracefully react to connection terminations from the remote end

--- client/main.go
+++ client/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"sync"
+	"time"
 )
 
 var addr = flag.String("addr", ":9999", "address to connect to")
@@ -13,17 +14,24 @@ var clients = flag.Int("clients", 3, "number of simultaneous connections")
 func connect(id int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	conn, err := net.Dial("tcp", *addr)
+	conn, err := net.DialTimeout("tcp", *addr, 5*time.Second)
 	if err != nil {
 		fmt.Printf("client %v could not connect to %q: %v\n", id, *addr, err)
 		return
 	}
 	defer conn.Close()
 
-	select {}
+	fmt.Printf("client %v connection %q->%q opened\n", id, conn.LocalAddr(), conn.RemoteAddr())
+
+	b := make([]byte, 10)
+	_, err = conn.Read(b)
+
+	fmt.Printf("client %v connection %q->%q closed: %v\n", id, conn.LocalAddr(), conn.RemoteAddr(), err)
 }
 
 func main() {
+	flag.Parse()
+
 	var wg sync.WaitGroup
 	for i := 0; i < *clients; i++ {
 		wg.Add(1)
